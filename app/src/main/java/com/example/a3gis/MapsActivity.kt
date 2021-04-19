b33d67953942a5205ba1991f2616f32c52296543
MussabayevSagatIT1901 <57889413+MussabayevSagatIT1901@users.noreply.github.com>
2021-04-19 16:24:38 +0600

    Update MapsActivity.kt

--- app/src/main/java/com/example/a3gis/MapsActivity.kt
+++ app/src/main/java/com/example/a3gis/MapsActivity.kt
@@ -3,6 +3,7 @@ package com.example.a3gis
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
